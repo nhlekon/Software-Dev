1f2ea35385fae4477dbb1b06bf11e9e857c5d9a4
Nhlanhla Nhleko <115709934+nhlekon@users.noreply.github.com>
2025-02-21 02:59:51 +0200

    Update C#.cs

--- C#.cs
+++ C#.cs
@@ -1,45 +1,120 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Shadowsocks.Controller;
-using GlobalHotKey;
-using System.Windows.Input;
-using System.Threading;
-using System.Collections.Generic;
-using Shadowsocks.Controller.Hotkeys;
-using System.Diagnostics;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
 
-
-namespace Shadowsocks.Test
+namespace CalculatorApp
 {
-    [TestClass]
-    public class UnitTest
+    public partial class CalculatorForm : Form
     {
-        [TestMethod]
-        public void TestHotKey2Str()
+        private string connectionString = "Server=your_server;Database=CalculatorDB;Trusted_Connection=True;";
+        private const string apiKey = "your_api_key";
+        private const string apiUrl = "https://api.exchangerate-api.com/v4/latest/";
+
+        public CalculatorForm()
         {
-            Assert.AreEqual("Ctrl+A", HotKeys.HotKey2Str(Key.A, ModifierKeys.Control));
-            Assert.AreEqual("Ctrl+Alt+D2", HotKeys.HotKey2Str(Key.D2, (ModifierKeys.Alt | ModifierKeys.Control)));
-            Assert.AreEqual("Ctrl+Alt+Shift+NumPad7", HotKeys.HotKey2Str(Key.NumPad7, (ModifierKeys.Alt | ModifierKeys.Control | ModifierKeys.Shift)));
-            Assert.AreEqual("Ctrl+Alt+Shift+F6", HotKeys.HotKey2Str(Key.F6, (ModifierKeys.Alt | ModifierKeys.Control | ModifierKeys.Shift)));
-            Assert.AreNotEqual("Ctrl+Shift+Alt+F6", HotKeys.HotKey2Str(Key.F6, (ModifierKeys.Alt | ModifierKeys.Control | ModifierKeys.Shift)));
+            InitializeComponent();
         }
 
-        [TestMethod]
-        public void TestStr2HotKey()
+        private void btnCalculate_Click(object sender, EventArgs e)
         {
-            Assert.IsTrue(HotKeys.Str2HotKey("Ctrl+A").Equals(new HotKey(Key.A, ModifierKeys.Control)));
-            Assert.IsTrue(HotKeys.Str2HotKey("Ctrl+Alt+A").Equals(new HotKey(Key.A, (ModifierKeys.Control | ModifierKeys.Alt))));
-            Assert.IsTrue(HotKeys.Str2HotKey("Ctrl+Shift+A").Equals(new HotKey(Key.A, (ModifierKeys.Control | ModifierKeys.Shift))));
-            Assert.IsTrue(HotKeys.Str2HotKey("Ctrl+Alt+Shift+A").Equals(new HotKey(Key.A, (ModifierKeys.Control | ModifierKeys.Alt | ModifierKeys.Shift))));
-            HotKey testKey0 = HotKeys.Str2HotKey("Ctrl+Alt+Shift+A");
-            Assert.IsTrue(testKey0 != null && testKey0.Equals(new HotKey(Key.A, (ModifierKeys.Control | ModifierKeys.Alt | ModifierKeys.Shift))));
-            HotKey testKey1 = HotKeys.Str2HotKey("Ctrl+Alt+Shift+F2");
-            Assert.IsTrue(testKey1 != null && testKey1.Equals(new HotKey(Key.F2, (ModifierKeys.Control | ModifierKeys.Alt | ModifierKeys.Shift))));
-            HotKey testKey2 = HotKeys.Str2HotKey("Ctrl+Shift+Alt+D7");
-            Assert.IsTrue(testKey2 != null && testKey2.Equals(new HotKey(Key.D7, (ModifierKeys.Control | ModifierKeys.Alt | ModifierKeys.Shift))));
-            HotKey testKey3 = HotKeys.Str2HotKey("Ctrl+Shift+Alt+NumPad7");
-            Assert.IsTrue(testKey3 != null && testKey3.Equals(new HotKey(Key.NumPad7, (ModifierKeys.Control | ModifierKeys.Alt | ModifierKeys.Shift))));
+            string expression = txtExpression.Text;
+            try
+            {
+                double result = EvaluateExpression(expression);
+                txtResult.Text = result.ToString();
+                SaveCalculation("Standard", expression, result.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid expression: " + ex.Message);
+            }
         }
 
+        private double EvaluateExpression(string expression)
+        {
+            return Convert.ToDouble(new DataTable().Compute(expression, null));
+        }
+
+        private void SaveCalculation(string type, string expression, string result)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO CalculationHistory (Type, Expression, Result) VALUES (@Type, @Expression, @Result)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Type", type);
+                cmd.Parameters.AddWithValue("@Expression", expression);
+                cmd.Parameters.AddWithValue("@Result", result);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void btnCalculateInterest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double principal = Convert.ToDouble(txtPrincipal.Text);
+                double rate = Convert.ToDouble(txtRate.Text) / 100;
+                int time = Convert.ToInt32(txtTime.Text);
+                double interest;
+
+                if (rbSimpleInterest.Checked)
+                {
+                    interest = principal * rate * time;
+                }
+                else
+                {
+                    interest = principal * Math.Pow((1 + rate), time) - principal;
+                }
+
+                txtInterestResult.Text = interest.ToString("F2");
+                SaveCalculation("Interest", $"P={principal}, R={rate*100}%, T={time}", interest.ToString("F2"));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid input: " + ex.Message);
+            }
+        }
+
+        private async void btnConvertCurrency_Click(object sender, EventArgs e)
+        {
+            string fromCurrency = cmbFromCurrency.SelectedItem.ToString();
+            string toCurrency = cmbToCurrency.SelectedItem.ToString();
+            double amount;
+            if (!double.TryParse(txtAmount.Text, out amount))
+            {
+                MessageBox.Show("Invalid amount");
+                return;
+            }
+
+            double rate = await GetExchangeRate(fromCurrency, toCurrency);
+            double convertedAmount = amount * rate;
+            txtConvertedResult.Text = convertedAmount.ToString("F2");
+            SaveCalculation("Currency", $"{amount} {fromCurrency} to {toCurrency}", convertedAmount.ToString("F2"));
+        }
+
+        private async Task<double> GetExchangeRate(string fromCurrency, string toCurrency)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string requestUrl = $"{apiUrl}{fromCurrency}?apikey={apiKey}";
+                HttpResponseMessage response = await client.GetAsync(requestUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    JObject json = JObject.Parse(responseBody);
+                    return json["rates"][toCurrency].Value<double>();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to fetch exchange rates.");
+                    return 0;
+                }
+            }
+        }
     }
 }
