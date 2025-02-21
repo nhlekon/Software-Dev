38a6cd0e8b885fd92fdfc91ecef5d6636c78229f
Nhlanhla Nhleko <115709934+nhlekon@users.noreply.github.com>
2025-02-21 12:01:30 +0200

    Update CarTradingCode.cs

--- CarTradingCode.cs
+++ CarTradingCode.cs
@@ -1,45 +1,145 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Shadowsocks.Controller;
-using GlobalHotKey;
-using System.Windows.Input;
-using System.Threading;
-using System.Collections.Generic;
-using Shadowsocks.Controller.Hotkeys;
-using System.Diagnostics;
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
-
-namespace Shadowsocks.Test
+namespace CarTradingApp
 {
-    [TestClass]
-    public class UnitTest
+    public partial class LoginForm : Form
     {
-        [TestMethod]
-        public void TestHotKey2Str()
+        private string connectionString = "Server=NHLANHLANHLEKO\SQLEXPRESS;Database=CarTradingDB;Trusted_Connection=True;";
+
+        public LoginForm()
         {
-            Assert.AreEqual("Ctrl+A", HotKeys.HotKey2Str(Key.A, ModifierKeys.Control));
-            Assert.AreEqual("Ctrl+Alt+D2", HotKeys.HotKey2Str(Key.D2, (ModifierKeys.Alt | ModifierKeys.Control)));
-            Assert.AreEqual("Ctrl+Alt+Shift+NumPad7", HotKeys.HotKey2Str(Key.NumPad7, (ModifierKeys.Alt | ModifierKeys.Control | ModifierKeys.Shift)));
-            Assert.AreEqual("Ctrl+Alt+Shift+F6", HotKeys.HotKey2Str(Key.F6, (ModifierKeys.Alt | ModifierKeys.Control | ModifierKeys.Shift)));
-            Assert.AreNotEqual("Ctrl+Shift+Alt+F6", HotKeys.HotKey2Str(Key.F6, (ModifierKeys.Alt | ModifierKeys.Control | ModifierKeys.Shift)));
+            InitializeComponent();
         }
 
-        [TestMethod]
-        public void TestStr2HotKey()
+        private void btnLogin_Click(object sender, EventArgs e)
         {
-            Assert.IsTrue(HotKeys.Str2HotKey("Ctrl+A").Equals(new HotKey(Key.A, ModifierKeys.Control)));
-            Assert.IsTrue(HotKeys.Str2HotKey("Ctrl+Alt+A").Equals(new HotKey(Key.A, (ModifierKeys.Control | ModifierKeys.Alt))));
-            Assert.IsTrue(HotKeys.Str2HotKey("Ctrl+Shift+A").Equals(new HotKey(Key.A, (ModifierKeys.Control | ModifierKeys.Shift))));
-            Assert.IsTrue(HotKeys.Str2HotKey("Ctrl+Alt+Shift+A").Equals(new HotKey(Key.A, (ModifierKeys.Control | ModifierKeys.Alt | ModifierKeys.Shift))));
-            HotKey testKey0 = HotKeys.Str2HotKey("Ctrl+Alt+Shift+A");
-            Assert.IsTrue(testKey0 != null && testKey0.Equals(new HotKey(Key.A, (ModifierKeys.Control | ModifierKeys.Alt | ModifierKeys.Shift))));
-            HotKey testKey1 = HotKeys.Str2HotKey("Ctrl+Alt+Shift+F2");
-            Assert.IsTrue(testKey1 != null && testKey1.Equals(new HotKey(Key.F2, (ModifierKeys.Control | ModifierKeys.Alt | ModifierKeys.Shift))));
-            HotKey testKey2 = HotKeys.Str2HotKey("Ctrl+Shift+Alt+D7");
-            Assert.IsTrue(testKey2 != null && testKey2.Equals(new HotKey(Key.D7, (ModifierKeys.Control | ModifierKeys.Alt | ModifierKeys.Shift))));
-            HotKey testKey3 = HotKeys.Str2HotKey("Ctrl+Shift+Alt+NumPad7");
-            Assert.IsTrue(testKey3 != null && testKey3.Equals(new HotKey(Key.NumPad7, (ModifierKeys.Control | ModifierKeys.Alt | ModifierKeys.Shift))));
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (AuthenticateUser(username, password, out string role, out string department))
+            {
+                MessageBox.Show("Login successful!");
+                MainForm mainForm = new MainForm(username, role, department);
+                mainForm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password.");
+            }
         }
 
+        private bool AuthenticateUser(string username, string password, out string role, out string department)
+        {
+            role = "";
+            department = "";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT Role, Department FROM Users WHERE Username = @Username AND PasswordHash = HASHBYTES('SHA2_256', @Password)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Username", username);
+                cmd.Parameters.AddWithValue("@Password", password);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    role = reader["Role"].ToString();
+                    department = reader["Department"].ToString();
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public partial class MainForm : Form
+    {
+        private string username;
+        private string role;
+        private string department;
+
+        public MainForm(string username, string role, string department)
+        {
+            InitializeComponent();
+            this.username = username;
+            this.role = role;
+            this.department = department;
+            LoadCars();
+        }
+
+        private void LoadCars()
+        {
+            using (SqlConnection conn = new SqlConnection("Server=your_server;Database=CarTradingDB;Trusted_Connection=True;"))
+            {
+                string query = role == "Manager" ? "SELECT * FROM Cars" : "SELECT * FROM Cars WHERE AddedBy IN (SELECT UserID FROM Users WHERE Department = @Department)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                if (role != "Manager")
+                {
+                    cmd.Parameters.AddWithValue("@Department", department);
+                }
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    lstCars.Items.Add($"{reader["CarID"]} - {reader["Make"]} {reader["Model"]} - {reader["Year"]} (${reader["Price"]}) - {reader["Status"]}");
+                }
+            }
+        }
+
+        private void btnRecordSale_Click(object sender, EventArgs e)
+        {
+            if (lstCars.SelectedItem != null)
+            {
+                using (SqlConnection conn = new SqlConnection("Server=your_server;Database=CarTradingDB;Trusted_Connection=True;"))
+                {
+                    string selectedCar = lstCars.SelectedItem.ToString();
+                    string[] carDetails = selectedCar.Split(' ');
+                    int carID = int.Parse(carDetails[0]);
+                    string buyerName = txtBuyerName.Text;
+                    decimal salePrice = decimal.Parse(txtSalePrice.Text);
+                    
+                    string query = "INSERT INTO Transactions (CarID, BuyerName, SalePrice, ProcessedBy) VALUES (@CarID, @BuyerName, @SalePrice, (SELECT UserID FROM Users WHERE Username = @Username));";
+                    string updateCarStatus = "UPDATE Cars SET Status = 'Sold' WHERE CarID = @CarID";
+                    
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@CarID", carID);
+                    cmd.Parameters.AddWithValue("@BuyerName", buyerName);
+                    cmd.Parameters.AddWithValue("@SalePrice", salePrice);
+                    cmd.Parameters.AddWithValue("@Username", username);
+                    
+                    SqlCommand cmdUpdate = new SqlCommand(updateCarStatus, conn);
+                    cmdUpdate.Parameters.AddWithValue("@CarID", carID);
+                    
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    cmdUpdate.ExecuteNonQuery();
+                    
+                    MessageBox.Show("Sale recorded successfully.");
+                    LoadCars();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a car to record the sale.");
+            }
+        }
+
+        private void btnViewSalesReport_Click(object sender, EventArgs e)
+        {
+            lstTransactions.Items.Clear();
+            using (SqlConnection conn = new SqlConnection("Server=your_server;Database=CarTradingDB;Trusted_Connection=True;"))
+            {
+                string query = "SELECT Cars.Make, Cars.Model, Cars.Year, Transactions.BuyerName, Transactions.SalePrice, Users.Username FROM Transactions INNER JOIN Cars ON Transactions.CarID = Cars.CarID INNER JOIN Users ON Transactions.ProcessedBy = Users.UserID";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    lstTransactions.Items.Add($"{reader["Make"]} {reader["Model"]} ({reader["Year"]}) - Buyer: {reader["BuyerName"]} - Price: ${reader["SalePrice"]} - Processed by: {reader["Username"]}");
+                }
+            }
+        }
     }
 }
